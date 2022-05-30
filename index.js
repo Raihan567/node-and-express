bd1c155502d3f30f8c80ce08951886069f77faec
Raihan567 <aburaihansarker567@gmail.com>
2022-05-31 03:40:50 +0600

    node and express server

---
+++ index.js
@@ -0,0 +1,146 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 8000;
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("<h1>hello, Now I am learning node.js and express.js</h1>");
+});
+
+const users = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Gwenborough",
+      zipcode: "92998-3874",
+      geo: {
+        lat: "-37.3159",
+        lng: "81.1496",
+      },
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+      name: "Romaguera-Crona",
+      catchPhrase: "Multi-layered client-server neural-net",
+      bs: "harness real-time e-markets",
+    },
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+    address: {
+      street: "Victor Plains",
+      suite: "Suite 879",
+      city: "Wisokyburgh",
+      zipcode: "90566-7771",
+      geo: {
+        lat: "-43.9509",
+        lng: "-34.4618",
+      },
+    },
+    phone: "010-692-6593 x09125",
+    website: "anastasia.net",
+    company: {
+      name: "Deckow-Crist",
+      catchPhrase: "Proactive didactic contingency",
+      bs: "synergize scalable supply-chains",
+    },
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+    address: {
+      street: "Douglas Extension",
+      suite: "Suite 847",
+      city: "McKenziehaven",
+      zipcode: "59590-4157",
+      geo: {
+        lat: "-68.6102",
+        lng: "-47.0653",
+      },
+    },
+    phone: "1-463-123-4447",
+    website: "ramiro.info",
+    company: {
+      name: "Romaguera-Jacobson",
+      catchPhrase: "Face to face bifurcated interface",
+      bs: "e-enable strategic applications",
+    },
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@kory.org",
+    address: {
+      street: "Hoeger Mall",
+      suite: "Apt. 692",
+      city: "South Elvis",
+      zipcode: "53919-4257",
+      geo: {
+        lat: "29.4572",
+        lng: "-164.2990",
+      },
+    },
+    phone: "493-170-9623 x156",
+    website: "kale.biz",
+    company: {
+      name: "Robel-Corkery",
+      catchPhrase: "Multi-tiered zero tolerance productivity",
+      bs: "transition cutting-edge web services",
+    },
+  },
+];
+//Use query parameter
+app.get("/users", (req, res) => {
+  const search = req.query.search;
+  if (search) {
+    const searchResult = users.filter((user) =>
+      user.name.toLocaleLowerCase().includes(search)
+    );
+    res.send(searchResult);
+  } else {
+    res.send(users);
+  }
+});
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+  newUser.id = users.length;
+  users.push(newUser);
+  res.send(`user heated on the page.`);
+  res.json(newUser)
+});
+
+// app.get("/users/:id", (req, res) => {
+//   const id = req.params.id;
+//   const user = users[id];
+//   res.send(user);
+// });
+
+// dynamic API
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const user = users[id];
+  res.send(user);
+});
+app.get("/fruits/mangoes/fazli", (req, res) => {
+  res.send("Yummy Yummy Fazli mangoes");
+});
+app.get("*", (_req, res) => {
+  res.send("404! page not found");
+});
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
+});
